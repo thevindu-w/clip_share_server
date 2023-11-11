f63fb96db51d7c3cf5aad9ce89b0baa1ce15caa8
thevindu-w <thevinduwijesekera99@gmail.com>
2023-11-11 23:38:05 +0530

    Add non-ASCII filename tests again with fix

---
+++ tests/scripts/1.4.1_send_file_v1.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+. init.sh
+
+fileName='文字檔案 1.txt'
+
+mkdir -p original && cd original
+
+echo "abc"$'\n'"$fileName"$'\n'"file content" >"$fileName"
+
+printf -v _ '%s%n' "$fileName" utf8nameLen
+nameLength="$(printf '%016x' $utf8nameLen)"
+fileSize=$(printf '%016x' $(stat -c '%s' "${fileName}"))
+content=$(cat "$fileName" | bin2hex | tr -d '\n')
+body="${nameLength}$(echo -n "$fileName" | bin2hex)${fileSize}${content}"
+
+cd ..
+mkdir -p copy
+update_config working_dir copy
+
+proto=$(printf '\x01' | bin2hex)
+method=$(printf '\x04' | bin2hex)
+
+responseDump=$(printf "${proto}${method}${body}" | hex2bin | client_tool | bin2hex | tr -d '\n')
+
+protoAck=$(printf '\x01' | bin2hex)
+methodAck=$(printf '\x01' | bin2hex)
+
+expected="${protoAck}${methodAck}"
+
+if [ "${responseDump}" != "${expected}" ]; then
+    showStatus info "Incorrect response."
+    echo 'Expected:' "$expected"
+    echo 'Received:' "$responseDump"
+    exit 1
+fi
+
+diffOutput=$(diff -rq original copy 2>&1 || echo failed)
+if [ ! -z "${diffOutput}" ]; then
+    showStatus info "File does not match."
+    exit 1
+fi
---
+++ tests/scripts/2.4.1_send_files_v2.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+. init.sh
+
+files=(
+    "文字檔案 1.txt"
+    "另一個文件_2.txt"
+    "資料夾_1/文字檔案 3.txt"
+    "資料夾_1/另一個文件 4.txt"
+    "資料夾 2/文字檔案 5.txt"
+    "資料夾 2/子資料夾/文字檔案 6.txt"
+    "資料夾 2/子資料夾/另一個文件_7.txt"
+    "資料夾 2/子資料夾 2/文字檔案 8.txt"
+)
+
+mkdir -p original && cd original
+
+for f in "${files[@]}"; do
+    if [[ $f == */* ]]; then
+        mkdir -p "${f%/*}"
+    fi
+    echo "${f}"$'\n'"abc" >"${f}"
+done
+
+chunks=""
+
+appendToChunks() {
+    fname="$1"
+    if [ -d "${fname}" ]; then
+        for f in "${fname}"/*; do
+            appendToChunks "${f}"
+        done
+    elif [ -f "${fname}" ]; then
+        printf -v _ '%s%n' "$fname" utf8nameLen
+        nameLength="$(printf '%016x' $utf8nameLen)"
+        fileSize=$(printf "%016x" $(stat -c '%s' "${fname}"))
+        content=$(cat "${fname}" | bin2hex | tr -d '\n')
+        chunks+="${nameLength}$(printf "${fname}" | bin2hex)${fileSize}${content}"
+    fi
+}
+
+for f in *; do
+    appendToChunks "${f}"
+done
+
+cd ..
+mkdir -p copies
+update_config working_dir copies
+
+proto=$(printf "\x02" | bin2hex)
+method=$(printf "\x04" | bin2hex)
+fileCount=$(printf "%016x" $(printf "${#files[@]}"))
+
+responseDump=$(printf "${proto}${method}${fileCount}${chunks}" | hex2bin | client_tool | bin2hex | tr -d '\n')
+
+protoAck=$(printf "\x01" | bin2hex)
+methodAck=$(printf "\x01" | bin2hex)
+
+expected="${protoAck}${methodAck}"
+
+if [ "${responseDump}" != "${expected}" ]; then
+    showStatus info "Incorrect response."
+    echo 'Expected:' "$expected"
+    echo 'Received:' "$responseDump"
+    exit 1
+fi
+
+diffOutput=$(diff -rq original copies 2>&1 || echo failed)
+if [ ! -z "${diffOutput}" ]; then
+    showStatus info "Files do not match."
+    exit 1
+fi
