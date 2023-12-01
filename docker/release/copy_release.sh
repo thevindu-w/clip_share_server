c766736a0e4f6e753278787a1ea404cd63bc2295
thevindu-w <thevinduwijesekera99@gmail.com>
2023-12-01 23:28:00 +0530

    Build release assets from workflow

---
+++ docker/release/copy_release.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ldd_head="$(ldd --version | head -n 1)"
+glibc_version=$(echo -n "${ldd_head##* }" | grep -oE '[0-9]+\.[0-9]+' | head -n 1)
+
+libssl_version="$(ldd clip_share | grep -oE 'libssl\.so\.[0-9]+' | head -n 1 | cut -d '.' -f 3)"
+
+mv -n clip_share "dist/clip_share_GLIBC-${glibc_version}_libssl-${libssl_version}" || echo 'Not replacing'
+mv -n clip_share_web "dist/clip_share_web_GLIBC-${glibc_version}_libssl-${libssl_version}" || echo 'Not replacing'
