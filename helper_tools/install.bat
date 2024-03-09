0b8390f6a861218e2b2debf208ff1421ff5807da
thevindu-w <thevinduwijesekera99@gmail.com>
2024-03-09 22:00:15 +0530

    Add install script for Windows

---
+++ helper_tools/install.bat
@@ -0,0 +1,38 @@
+@ECHO OFF
+
+IF NOT EXIST clip_share*.exe (
+    ECHO 'clip_share.exe' file does not exist.
+    ECHO Please download the 'clip_share.exe' Windows version and place it in this folder.
+    ECHO Install failed.
+    PAUSE
+    EXIT
+)
+
+ECHO This will install clip_share to run on startup.
+SET /P confirm=Proceed? [y/n] 
+IF /I NOT "%confirm%" == "y" (
+    ECHO Aborted.
+    ECHO You can still use clip_share by manually running the program.
+    PAUSE
+    EXIT
+)
+
+RENAME clip_share*.exe clip_share.exe
+MKDIR "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\" >NUL 2>&1
+COPY clip_share.exe "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\clip_share.exe" >NUL 2>&1
+
+CD %USERPROFILE%
+IF NOT EXIST clipshare.conf (
+    MKDIR "%USERPROFILE%\Downloads" >NUL 2>&1
+    ECHO working_dir=%USERPROFILE%\Downloads>clipshare.conf
+    ECHO Created new configuration file %USERPROFILE%\clipshare.conf
+)
+ECHO Installed ClipShare to run on startup.
+
+SET /P start_now=Start ClipShare now? [y/n] 
+IF /I "%start_now%" == "y" (
+    START "" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\clip_share.exe"
+    ECHO Started ClipShare server.
+)
+PAUSE
+EXIT
