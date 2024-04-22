b35c8043cbc178407df0f041d37faec7adaeb77b
thevindu-w <thevinduwijesekera99@gmail.com>
2024-04-22 21:01:02 +0530

    Stop including web mode binaries in releases

--- docker/release/copy_release.sh
+++ docker/release/copy_release.sh
@@ -6,4 +6,3 @@ glibc_version=$(echo -n "${ldd_head##* }" | grep -oE '[0-9]+\.[0-9]+' | head -n 
 libssl_version="$(ldd clip_share | grep -oE 'libssl\.so\.[0-9]+' | head -n 1 | cut -d '.' -f 3)"
 
 mv -n clip_share "dist/clip_share_GLIBC-${glibc_version}_libssl-${libssl_version}" || echo 'Not replacing'
-mv -n clip_share_web "dist/clip_share_web_GLIBC-${glibc_version}_libssl-${libssl_version}" || echo 'Not replacing'
