843eb8a83f1450abbe5c9b6855c09e64a5db6f1d
thevindu-w <thevinduwijesekera99@gmail.com>
2025-03-26 23:34:29 +0530

    Option to encrypt CA key after keygen

--- helper_tools/keygen.sh
+++ helper_tools/keygen.sh
@@ -28,23 +28,23 @@ export MSYS2_ARG_CONV_EXCL='/CN'
 echo 'Generating keys and certificates ...'
 # generate CA keys
 openssl genrsa -out ca.key 4096 >keygen.log 2>&1
-openssl req -x509 -new -nodes -key ca.key -sha256 -days 1826 -out ca.crt -reqexts v3_req -extensions v3_ca -subj '/CN=clipshare_ca'
+openssl req -x509 -new -nodes -key ca.key -sha256 -days 1826 -out ca.crt -reqexts v3_req -extensions v3_ca -subj '/CN=clipshare_ca' >>keygen.log 2>&1
 
 # generate server keys
 openssl genrsa -out server.key 2048 >>keygen.log 2>&1
-openssl req -new -key server.key -out server.csr -subj "/CN=$SERVER_NAME"
+openssl req -new -key server.key -out server.csr -subj "/CN=$SERVER_NAME" >>keygen.log 2>&1
 openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 730 -sha256 -extfile clipshare.ext >>keygen.log 2>&1
 
 # export keys to pkcs12 file
 if type winpty &>/dev/null; then
-    winpty openssl pkcs12 -export -in server.crt -inkey server.key -passout pass: -out server.pfx
+    winpty openssl pkcs12 -export -in server.crt -inkey server.key -passout pass: -out server.pfx >>keygen.log 2>&1
 else
-    openssl pkcs12 -export -in server.crt -inkey server.key -passout pass: -out server.pfx
+    openssl pkcs12 -export -in server.crt -inkey server.key -passout pass: -out server.pfx >>keygen.log 2>&1
 fi
 
 # generate client keys
 openssl genrsa -out client.key 2048 >>keygen.log 2>&1
-openssl req -new -key client.key -out client.csr -subj "/CN=$CLIENT_NAME"
+openssl req -new -key client.key -out client.csr -subj "/CN=$CLIENT_NAME" >>keygen.log 2>&1
 openssl x509 -req -in client.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out client.crt -days 730 -sha256 -extfile clipshare.ext >>keygen.log 2>&1
 
 echo 'Generated keys and certificates.'
@@ -63,10 +63,26 @@ fi
 echo
 echo 'Exported client keys.'
 echo 'Cleaning up ...'
-rm -f *.srl *.csr
-rm client.key client.crt server.key server.crt
-
+rm -f *.srl *.csr >>keygen.log 2>&1
+rm client.key client.crt server.key server.crt >>keygen.log 2>&1
 echo 'Done.'
+
+echo
+echo 'If you do not plan to create more keys in the future, you may safely delete the "ca.key" file. Otherwise, store the "ca.key" file securely.'
+read -p 'Do you plan to create more keys and like to encrypt the "ca.key" file to secure it? [y/N] ' confirm
+if [ "${confirm::1}" = 'y' ] || [ "${confirm::1}" = 'Y' ]; then
+    echo 'Encrypting ca.key ...'
+    echo
+    echo 'Please enter a password that you can remember. You will need this password when using the key to sign more certificates in the future.'
+    echo
+    openssl pkcs8 -topk8 -scrypt -in ca.key -out ca.enc.key
+    rm ca.key >>keygen.log 2>&1
+    echo 'Encrypted ca.key.'
+else
+    echo
+    echo 'The "ca.key" file is not encrypted. If you do not plan to create more keys, please delete that file for security.'
+fi
+
 echo
 echo '> server.pfx - The TLS key and certificate store file of the server. Keep this file securely.'
 echo '> ca.crt     - The TLS certificate file of the CA. You need this file on both the server and the client devices.'
@@ -76,5 +92,6 @@ echo
 echo "# the server's name is $SERVER_NAME"
 echo "# the client's name is $CLIENT_NAME"
 echo
-echo 'Note: If you do not plan to create more keys in the future, you may safely delete the "ca.key" file. Otherwise, store the "ca.key" file securely.'
+echo 'Note: If you do not plan to create more keys in the future, you may safely delete the "ca.key" file. Otherwise, if you did not encrypt it, store the "ca.key" file securely.'
+
 rm -f keygen.log
