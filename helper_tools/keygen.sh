607d1fe310cbae9b49977dcb473c2e5fe9e615e8
thevindu-w <thevinduwijesekera99@gmail.com>
2025-04-05 23:29:23 +0530

    Update keygen.sh script to use existing CA file

--- helper_tools/keygen.sh
+++ helper_tools/keygen.sh
@@ -23,65 +23,130 @@ if [ ! -f clipshare.ext ]; then
     exit 1
 fi
 
+create_encrypted_pfx() {
+    local name="$1"
+    if type winpty &>/dev/null; then
+        winpty openssl pkcs12 -export -in "${name}.crt" -inkey "${name}.key" -out "${name}.pfx"
+    else
+        openssl pkcs12 -export -in "${name}.crt" -inkey "${name}.key" -out "${name}.pfx"
+    fi
+}
+
+extract_ca_pem() {
+    openssl x509 -in ca.pem -out ca.crt
+    openssl rsa -in ca.pem -out ca.key >>keygen.log 2>&1
+    rm ca.pem >>keygen.log 2>&1
+}
+
 export MSYS2_ARG_CONV_EXCL='/CN'
 
-echo 'Generating keys and certificates ...'
-# generate CA keys
-openssl genrsa -out ca.key 4096 >keygen.log 2>&1
-openssl req -x509 -new -nodes -key ca.key -sha256 -days 1826 -out ca.crt -reqexts v3_req -extensions v3_ca -subj '/CN=clipshare_ca' >>keygen.log 2>&1
-
-# generate server keys
-openssl genrsa -out server.key 2048 >>keygen.log 2>&1
-openssl req -new -key server.key -out server.csr -subj "/CN=$SERVER_NAME" >>keygen.log 2>&1
-openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 730 -sha256 -extfile clipshare.ext >>keygen.log 2>&1
-
-# export keys to pkcs12 file
-if type winpty &>/dev/null; then
-    winpty openssl pkcs12 -export -in server.crt -inkey server.key -passout pass: -out server.pfx >>keygen.log 2>&1
-else
-    openssl pkcs12 -export -in server.crt -inkey server.key -passout pass: -out server.pfx >>keygen.log 2>&1
+reused_ca=0
+encrypted_ca=0
+if [ -f ca.pfx ] || [ -f ca.enc.pfx ]; then
+    echo 'Found previously created CA file.'
+    read -p 'Use it? [Y/n] ' confirm
+    if [ "${confirm::1}" != 'n' ] && [ "${confirm::1}" != 'N' ]; then
+        echo
+        if [ -f ca.enc.pfx ]; then
+            echo 'Please enter the password for ca.enc.pfx file.'
+            if type winpty &>/dev/null; then
+                winpty openssl pkcs12 -in ca.enc.pfx -nodes -out ca.pem
+            else
+                openssl pkcs12 -in ca.enc.pfx -nodes -out ca.pem
+            fi
+            encrypted_ca=1
+        else
+            if type winpty &>/dev/null; then
+                winpty openssl pkcs12 -in ca.pfx -passin pass: -nodes -out ca.pem
+            else
+                openssl pkcs12 -in ca.pfx -passin pass: -nodes -out ca.pem
+            fi
+        fi
+        extract_ca_pem
+        echo 'Extracted CA key and certificate.'
+        reused_ca=1
+    fi
 fi
 
-# generate client keys
-openssl genrsa -out client.key 2048 >>keygen.log 2>&1
-openssl req -new -key client.key -out client.csr -subj "/CN=$CLIENT_NAME" >>keygen.log 2>&1
-openssl x509 -req -in client.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out client.crt -days 730 -sha256 -extfile clipshare.ext >>keygen.log 2>&1
-
-echo 'Generated keys and certificates.'
-echo
-echo 'Exporting client keys ...'
-# export keys to pkcs12 file
-echo 'Please enter a password that you can remember. You will need this password when importing the key on the client.'
-echo
-
-if type winpty &>/dev/null; then
-    winpty openssl pkcs12 -export -in client.crt -inkey client.key -out client.pfx
-else
-    openssl pkcs12 -export -in client.crt -inkey client.key -out client.pfx
+if [ "$reused_ca" != 1 ]; then
+    echo 'Generating CA key and certificate ...'
+    openssl genrsa -out ca.key 4096 >keygen.log 2>&1
+    openssl req -x509 -new -nodes -key ca.key -sha256 -days 1826 -out ca.crt -reqexts v3_req -extensions v3_ca -subj '/CN=clipshare_ca' >>keygen.log 2>&1
 fi
 
-echo
-echo 'Exported client keys.'
+skip_server=0
+if [ -f server.pfx ]; then
+    skip_server=1
+    echo
+    echo 'Found existing server.pfx.'
+    read -p 'Overwrite it to create a new server key and certificate? [y/N] ' confirm
+    if [ "${confirm::1}" = 'y' ] || [ "${confirm::1}" = 'Y' ]; then
+        skip_server=0
+    fi
+fi
+if [ "$skip_server" = 0 ]; then
+    openssl genrsa -out server.key 2048 >>keygen.log 2>&1
+    openssl req -new -key server.key -out server.csr -subj "/CN=$SERVER_NAME" >>keygen.log 2>&1
+    openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 730 -sha256 -extfile clipshare.ext >>keygen.log 2>&1
+    echo 'Generated server keys and certificates.'
+    if type winpty &>/dev/null; then
+        winpty openssl pkcs12 -export -in server.crt -inkey server.key -passout pass: -out server.pfx >>keygen.log 2>&1
+    else
+        openssl pkcs12 -export -in server.crt -inkey server.key -passout pass: -out server.pfx >>keygen.log 2>&1
+    fi
+fi
+
+skip_client=0
+if [ -f client.pfx ]; then
+    skip_client=1
+    echo
+    echo 'Found existing client.pfx.'
+    read -p 'Overwrite it to create a new client key and certificate? [y/N] ' confirm
+    if [ "${confirm::1}" = 'y' ] || [ "${confirm::1}" = 'Y' ]; then
+        skip_client=0
+    fi
+fi
+if [ "$skip_client" = 0 ]; then
+    openssl genrsa -out client.key 2048 >>keygen.log 2>&1
+    openssl req -new -key client.key -out client.csr -subj "/CN=$CLIENT_NAME" >>keygen.log 2>&1
+    openssl x509 -req -in client.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out client.crt -days 730 -sha256 -extfile clipshare.ext >>keygen.log 2>&1
+    echo 'Generated client keys and certificates.'
+    echo
+    echo 'Exporting client keys ...'
+    echo 'Please enter a password that you can remember. You will need this password when importing the key on the client.'
+    echo
+    create_encrypted_pfx client
+    echo
+    echo 'Exported client keys.'
+fi
 echo 'Cleaning up ...'
 rm -f *.srl *.csr >>keygen.log 2>&1
-rm client.key client.crt server.key server.crt >>keygen.log 2>&1
+rm -f client.key client.crt server.key server.crt >>keygen.log 2>&1
 echo 'Done.'
 
-echo
-echo 'If you do not plan to create more keys in the future, you may safely delete the "ca.key" file. Otherwise, store the "ca.key" file securely.'
-read -p 'Do you plan to create more keys and like to encrypt the "ca.key" file to secure it? [y/N] ' confirm
-if [ "${confirm::1}" = 'y' ] || [ "${confirm::1}" = 'Y' ]; then
-    echo 'Encrypting ca.key ...'
+if [ "$reused_ca" != 1 ]; then
     echo
-    echo 'Please enter a password that you can remember. You will need this password when using the key to sign more certificates in the future.'
-    echo
-    openssl pkcs8 -topk8 -scrypt -in ca.key -out ca.enc.key
-    rm ca.key >>keygen.log 2>&1
-    echo 'Encrypted ca.key.'
-else
-    echo
-    echo 'The "ca.key" file is not encrypted. If you do not plan to create more keys, please delete that file for security.'
+    read -p 'Do you plan to create more keys in the future and like to encrypt the CA key file to secure it? [y/N] ' confirm
+    if [ "${confirm::1}" = 'y' ] || [ "${confirm::1}" = 'Y' ]; then
+        echo 'Encrypting CA key ...'
+        echo
+        echo 'Please enter a password that you can remember. You will need this password when using the key to sign more certificates in the future.'
+        echo
+        create_encrypted_pfx ca
+        mv ca.pfx ca.enc.pfx >>keygen.log 2>&1
+        echo 'Stored the encrypted CA key in "ca.enc.pfx"'
+        encrypted_ca=1
+    else
+        if type winpty &>/dev/null; then
+            winpty openssl pkcs12 -export -in ca.crt -inkey ca.key -passout pass: -out ca.pfx >>keygen.log 2>&1
+        else
+            openssl pkcs12 -export -in ca.crt -inkey ca.key -passout pass: -out ca.pfx >>keygen.log 2>&1
+        fi
+        echo
+        echo 'The CA key is stored in "ca.pfx" without encrypting. If you do not plan to create more keys, please delete that file for security.'
+    fi
 fi
+rm ca.key >>keygen.log 2>&1
 
 echo
 echo '> server.pfx - The TLS key and certificate store file of the server. Keep this file securely.'
@@ -91,7 +156,9 @@ echo '> client.pfx - The TLS key and certificate store file of the client. Move 
 echo
 echo "# the server's name is $SERVER_NAME"
 echo "# the client's name is $CLIENT_NAME"
-echo
-echo 'Note: If you do not plan to create more keys in the future, you may safely delete the "ca.key" file. Otherwise, if you did not encrypt it, store the "ca.key" file securely.'
+if [ "$encrypted_ca" = 0 ]; then
+    echo
+    echo 'Note: If you do not plan to create more keys in the future, you may safely delete the "ca.pfx" file. Otherwise, store the file securely.'
+fi
 
 rm -f keygen.log
