9e8247cc069e30c0ed8ec05e7a9a95d546b6c4b4
thevindu-w <thevinduwijesekera99@gmail.com>
2025-07-11 23:53:27 +0530

    Build and test on Linux ARMv7

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -2,9 +2,11 @@
 
 set -e
 
-# start the Xvfb virtual display
-Xvfb :0 -screen 0 1280x720x8 >/dev/null 2>&1 &
-sleep .1
+if type Xvfb &>/dev/null; then
+    # start the Xvfb virtual display
+    Xvfb :0 -screen 0 1280x720x8 >/dev/null 2>&1 &
+    sleep .1
+fi
 
 # run the tests
 make test
